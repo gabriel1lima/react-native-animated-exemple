fcb21b041f567bb92fe16ad1ea90907da4813846
Gabriel Lima <gabriel.lima@aluno.fapce.edu.br>
2019-07-17 15:49:36 -0300

    Deixando invisivel a informação do rodapé quando abre a sacola por completo

--- src/components/Basket/index.js
+++ src/components/Basket/index.js
@@ -1,10 +1,19 @@
 import React, { Component, useState } from 'react';
-import { Animated, View, Text, StatusBar, StyleSheet } from 'react-native';
+import { Animated, View, Text, StatusBar, StyleSheet, Dimensions } from 'react-native';
 import { PanGestureHandler, State, TouchableOpacity } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/Feather';
 import styles from './style';
 
 export default function Basket(props) {
+
+  // const window = Dimensions.get('window').height;
+  // let height = -Math.abs((77 * window) / 100);
+
+  // height = -430
+
+  // console.tron.log(window)
+  // console.tron.log(height)
+
   const [visible, setVisible] = useState(false);
 
   const translateY = new Animated.Value(0);
@@ -92,28 +101,51 @@ export default function Basket(props) {
                     extrapolate: 'clamp',
                   }),
                 }],
-              }, 
+                // borderTopLeftRadius: translateY.interpolate({
+                //   inputRange: [-430, 0],
+                //   outputRange: [0, 20],
+                //   extrapolate: 'clamp',
+                // }),
+                // borderTopRightRadius: translateY.interpolate({
+                //   inputRange: [-430, 0],
+                //   outputRange: [0, 20],
+                //   extrapolate: 'clamp',
+                // }),
+              },
             ]}
           >
+            {
+              <Animated.View style={{width: '100%', height: '12.5%', backgroundColor: '#d3222a', opacity: translateY.interpolate({
+                inputRange: [-430 ,0],
+                outputRange: [0, 1],
+                extrapolate: 'clamp',
+              }),}}>
+                <View style={{width: 40, height: 5, backgroundColor: 'rgba(255,255,255,.8)', marginTop: 10, borderRadius: 2.5, alignSelf: 'center'}}></View>
 
-            <View style={{width: '100%', height: '12.5%', backgroundColor: '#d3222a'}}>
-              <View style={{width: 40, height: 5, backgroundColor: 'rgba(255,255,255,.8)', marginTop: 10, borderRadius: 2.5, alignSelf: 'center'}}></View>
-
-              <View style={{marginHorizontal: 15, marginTop: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-                
-                <View style={{position: 'absolute', left: 0}}>
-                  <Icon name="shopping-bag" size={23} color="#FFF" />
-                  <View style={{width: 15, height: 15, backgroundColor: 'white', borderRadius: 7.5, position: 'absolute', right: -6, top: -4, justifyContent: 'center', alignItems: 'center'}}>
-                    <Text style={{color: '#d3222a', fontSize: 10, fontWeight: 'bold'}}>1</Text>
+                <View style={{marginHorizontal: 15, marginTop: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                  
+                  <View style={{position: 'absolute', left: 0}}>
+                    <Icon name="shopping-bag" size={23} color="#FFF" />
+                    <View style={{width: 15, height: 15, backgroundColor: 'white', borderRadius: 7.5, position: 'absolute', right: -6, top: -4, justifyContent: 'center', alignItems: 'center'}}>
+                      <Text style={{color: '#d3222a', fontSize: 10, fontWeight: 'bold'}}>1</Text>
+                    </View>
                   </View>
+                  
+                  
+                  <Text style={{fontWeight: 'bold', color: 'white'}}>{props.nome}</Text>
+                  <Text style={{fontWeight: 'bold', color: 'white', position: 'absolute', right: 0}}>R$ 22,00</Text>
                 </View>
-                
-                
-                <Text style={{fontWeight: 'bold', color: 'white'}}>{props.nome}</Text>
-                <Text style={{fontWeight: 'bold', color: 'white', position: 'absolute', right: 0}}>R$ 22,00</Text>
-              </View>
-            </View>
-            
+              </Animated.View>
+            }
+
+            <Animated.View style={{width: '100%', alignItems: 'center',  position: 'absolute', opacity: translateY.interpolate({
+                inputRange: [-430, -150],
+                outputRange: [1, 0],
+                extrapolate: 'clamp',
+              }),}}>
+              <Text>OPA</Text>
+            </Animated.View>
+
           </Animated.View>
         </PanGestureHandler>
       </View>
